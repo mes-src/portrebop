1b332a3e524183cabaa236d700ab780dc680c257
Michael Sands <michaelesands@gmail.com>
2021-04-22 00:39:21 -0400

    performance & attribution

--- Attribution/quantopian/tearsheet_basics.py
+++ Attribution/quantopian/tearsheet_basics.py
@@ -6,8 +6,6 @@ from datetime import date
 
 today = date.today()
 
-
-
 returns = yf.download('AAPL','1975-01-01',today)['Adj Close'].resample('M').last().pct_change()
 print(returns.head(2))
 pf.create_returns_tear_sheet(returns, live_start_date='2015-12-1')
